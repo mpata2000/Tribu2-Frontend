816055ac25206370bf3810df696b2886c3d5206a
jpdicomo <jdicomo@fi.uba.ar>
2021-12-13 14:46:55 -0300

    Added navigation to sidebar

---
+++ src/containers/HoursContainer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import HoursView from 'views/Hours';
+
+const HoursContainer = () => {
+    
+    return (
+        <HoursView/>    
+    )
+}
+
+export default HoursContainer;
