0bd58b26e926b9f22db3077f701c5db0b988c598
tomyrd <trodriguez@fi.uba.ar>
2021-12-16 20:20:37 -0300

    Ahora los tickets que se muestran dependen del producto que se eligio

--- src/containers/SoporteContainer.tsx
+++ src/containers/SoporteContainer.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react';
 import SoporteView from 'views/Soporte'
 import {useDispatch} from 'react-redux'
 import { onTicketsGetAll } from 'redux_folder/actions/tickets.actions';
@@ -6,13 +6,15 @@ import useTypedSelector from 'hooks/useTypedSelector';
 
 const SoporteContainer = () => {
     
+    const products = [{"name":"Siu Guarani","versions":["1.0.0"]},{"name":"Proyecto 2","versions":["2.0","2.1","2.1.1"]}];
     
     const dispatch = useDispatch();
 
-    const onGetTickets = () => {
-        dispatch(onTicketsGetAll("Siu Guarani", "1.0.0"));
+    const onGetTickets = (product:string, version:string) => {
+        dispatch(onTicketsGetAll(product, version));
     }
-    return (<SoporteView onGetTickets={onGetTickets}/>);
+
+    return (<SoporteView onGetTickets={onGetTickets} products ={products} />);
 }
 
 export default SoporteContainer
