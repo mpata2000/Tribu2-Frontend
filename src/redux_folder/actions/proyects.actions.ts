cd3910d53347243c09dab35d09840a1b0f61f2bd
cessar5 <cessar5@gmail.com>
2021-12-16 18:08:12 -0300

    agregado de POST y DELETE

--- src/redux_folder/actions/proyects.actions.ts
+++ src/redux_folder/actions/proyects.actions.ts
@@ -4,9 +4,19 @@ import {AnyAction} from 'redux'
 // PROYECTS_ON_GET_ALL
 export const onProyectsGetAll = ():  AnyAction => (
   { type: constants.PROYECTS_ON_GET_ALL_REQUESTED});
-
+  
 export const onGetAllSucceeded = (data: any): AnyAction => (
   { type: constants.PROYECTS_ON_GET_ALL_SUCCEEDED, data });
 
 export const onGetAllFailed = (error: unknown): AnyAction => (
   { type: constants.PROYECTS_ON_GET_ALL_FAILED, error });
+  
+
+export const createProyect = (data: any):  AnyAction => (
+    { type: constants.PROYECTS_ON_CREATE_REQUESTED, data});
+  
+export const createProyectSucceeded = (data: any): AnyAction => (
+    { type: constants.PROYECTS_ON_CREATE_SUCCEEDED, data });
+  
+export const createProyectFailed = (error: unknown): AnyAction => (
+    { type: constants.PROYECTS_ON_CREATE_FAILED, error });
