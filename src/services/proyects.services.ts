04421e3509055b7224f50341ab07a94997e3d6ae
inazario <gretty.nazario@gmail.com>
2021-12-17 20:53:38 -0300

    test con git

--- src/services/proyects.services.ts
+++ src/services/proyects.services.ts
@@ -9,23 +9,8 @@ export async function getProyects(): Promise<any> {
 }
 
 export async function postProyects(proyecto: any): Promise<any> {
-  debugger;
-  const projectData = {
-    nombre: "HOLA MUNDO",
-    descripcion: "hola mundo",
-    fechaInicioReal: "2021-10-12",
-    fechaFinalizacionReal: "2021-10-12",
-    fechaInicioEstimada: "2021-10-12",
-    fechaFinalizacionEstimada: "2021-10-12",
-    fechaEntregaComunicadaACliente: "2021-10-12",
-    horasEstimadas: 0,
-    prioridad: "Baja",
-    estado: 'No iniciada'
-}
-  console.log(proyecto.data);
   const response = await post(`${url}/proyectos`, proyecto.data);
   return response;
-  // return null;
 }
 
 export async function saveProyects(): Promise<any> {
@@ -36,3 +21,4 @@ export async function saveProyects(): Promise<any> {
    const response = await delete_(`${url}/proyectos/`+id);
    return response;
  }
+
