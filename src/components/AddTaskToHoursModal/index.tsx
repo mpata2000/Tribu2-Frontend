afdeea2092e59e8b2d75d97107fecf4fd0ef8d73
jpdicomo <jdicomo@fi.uba.ar>
2021-12-18 00:52:31 -0300

    Added hours creation

--- src/components/AddTaskToHoursModal/index.tsx
+++ src/components/AddTaskToHoursModal/index.tsx
@@ -1,12 +1,51 @@
-import { Box, Typography } from '@mui/material';
+import { Alert, Box, FormControl, InputLabel, MenuItem, Select, TextField, Typography } from '@mui/material';
 import { format } from 'date-fns';
+import useTypedSelector from 'hooks/useTypedSelector';
 import React from 'react'
 import './index.css'
+import { Controller, useForm } from 'react-hook-form';
+import { useDispatch } from 'react-redux';
+import { onCreateHours } from 'redux_folder/actions/hours.actions';
 
 const AddTaskToHoursModal = (props: any) => {
 
     const {date} = props;
 
+    const {
+        handleSubmit, control, formState: { errors },
+      } = useForm();
+    
+    const dispatch = useDispatch()
+    const tasks = [
+        {
+          "idTarea": 'XD PATA',
+          "idProyecto": {
+            "idProyecto": 0,
+            "idLegajo": 0,
+            "nombre": "Proyecto 1",
+            "descripcion": "string",
+            "fechaInicioReal": "2021-12-17",
+            "fechaFinalizacionReal": "2021-12-17",
+            "fechaInicioEstimada": "2021-12-17",
+            "fechaFinalizacionEstimada": "2021-12-17",
+            "fechaEntregaComunicadaACliente": "2021-12-17",
+            "horasEstimadas": 0,
+            "prioridad": "string",
+            "estado": "string"
+          },
+          "idLegajo": 0,
+          "nombre": "Tarea 1",
+          "descripcion": "string",
+          "fechaInicioReal": "2021-12-17",
+          "fechaFinalizacionReal": "2021-12-17",
+          "horasEstimadas": 0,
+          "horasTrabajadas": 0,
+          "idTicket": 0,
+          "prioridad": "string",
+          "estado": "string"
+        }
+      ]
+      
     const style = {
         position: 'absolute' as 'absolute',
         top: '50%',
@@ -18,16 +57,130 @@ const AddTaskToHoursModal = (props: any) => {
         boxShadow: 24,
         p: 4,
       };
+    
+    const onSubmit = (data:any)=>{
+        let data_ = {
+            ...data,
+            user_id: 102889,
+            day: format(date, 'yyyy-MM-dd'),
+        }
+        if(!data.note){
+            data_ = {
+                ...data_,
+                note: '',
+            }
+        }
 
+        if(!data.hours){
+            data_ = {
+                ...data_,
+                hours: '0',
+            }
+        }
+        if(!data.minutes){
+            data_ = {
+                ...data_,
+                minutes: '0',
+            }
+        }
+        if(!data.seconds){
+            data_ = {
+                ...data_,
+                seconds: '0',
+            }
+        }
+        dispatch(onCreateHours(data_))
+        
+
+    }
     return(
         <Box sx={style}>
-                <Typography id="modal-modal-title" variant="h6" component="h2">
-                    New time entry for {format(date,'eeee MM/dd/yyyy')}
+                <Typography id="title" variant="h6" component="h2">
+                    New time entry for {format(date,'eeee dd/MM/yyyy')}
                 </Typography>
-                <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                    Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-                </Typography>
-                </Box>
+                <form onSubmit={handleSubmit(onSubmit)} style={{display: 'flex', flexDirection:'column', justifyContent:'center'}}>
+                <Controller
+                    control={control}
+                    render={({ field: { onChange, onBlur, value } }) => {
+                        return (
+                            <>
+                                <InputLabel id="tasks"> Tasks </InputLabel>
+                                <Select
+                                    labelId="tasks"
+                                    id="tasks"
+                                    value={value}
+                                    label="Tasks"
+                                    onChange={onChange}
+                                    style={{minWidth: '80%'}}
+                                >
+                                    {tasks.map((task) => <MenuItem value={task.idTarea}> {task.nombre} from {task.idProyecto.nombre}</MenuItem>)}
+                                </Select>
+                            </>
+                        )}}
+                    name="task_id"
+                    rules={{ required: true }}
+                />
+                {errors?.tasks?.type === 'required' && <p style={{color: 'red', fontSize: 10}}>Required</p>}
+                <Controller
+                    control={control}
+                    render={({ field: { onChange, onBlur, value } }) => {
+                        return (
+                            <>
+                                <InputLabel id="note"/>
+                                <TextField id="note"  placeholder='Note'  variant="outlined" onChange={onChange}/>
+                            </>
+                        )}}
+                    name="note"
+                />
+                <div style={{display: 'flex' , justifyContent: 'center'}}>
+                            
+                    <Controller
+                        control={control}
+                        render={({ field: { onChange, onBlur, value } }) => {
+                            return (
+                                <>
+                                    <InputLabel id="hours"/>
+                                    <TextField id="hours"  placeholder='00'  variant="standard" onChange={onChange}/>
+                                </>
+                            )}}
+                        name="hours"
+                        rules={{pattern: /[0-9]*/}}
+                    />
+                    <p style={{alignSelf: 'center', margin: 10}}>:</p>
+                        
+                    <Controller
+                        control={control}
+                        render={({ field: { onChange, onBlur, value } }) => {
+                            return (
+                                <>
+                                    <InputLabel id="minutes"/>
+                                    <TextField id="minutes"  placeholder='00'  variant="standard" onChange={onChange}/>
+                                </>
+                                )}}
+                        name="minutes"
+                        rules={{pattern: /[0-9]*/}}
+                    />
+                    <p style={{alignSelf: 'center', margin: 10}}>:</p>
+                    <Controller
+                        control={control}
+                        render={({ field: { onChange, onBlur, value } }) => {
+                            return (
+                                <>
+                                    <InputLabel id="seconds"/>
+                                    <TextField id="seconds"  placeholder='00'  variant="standard" onChange={onChange}/>
+                                </>
+                                )}}
+                        name="seconds"
+                        rules={{pattern: /[0-9]*/}}
+                    />
+                            
+                </div>  
+                {errors?.hours?.type === 'pattern' && <p style={{color: 'red', fontSize: 10}}>Pls enter a number</p>}
+                {errors?.minutes?.type === 'pattern' && <p style={{color: 'red', fontSize: 10}}>Pls enter a number</p>}
+                {errors?.seconds?.type === 'pattern' && <p style={{color: 'red', fontSize: 10}}>Pls enter a number</p>}
+                <input type="submit" />
+                </form>    
+            </Box>
     )
 }
 
--- src/redux_folder/sagas/hours.sagas.ts
+++ src/redux_folder/sagas/hours.sagas.ts
@@ -5,7 +5,7 @@ import {
 import * as constants from 'redux_folder/constants/hours.constants';
 import * as actions from 'redux_folder/actions/hours.actions';
 
-import { getHours } from 'services/hours.services';
+import { createHours, getHours } from 'services/hours.services';
 
 export function* hoursGetAll() {
   try {
@@ -15,10 +15,18 @@ export function* hoursGetAll() {
     yield put(actions.onGetAllFailed(error));
   }
 }
-
+export function* hoursCreate(action: any) {
+  try {
+    const data: unknown = yield call(createHours, action.data_);
+    yield put(actions.onCreateHoursSucceeded(data));
+  } catch (error) {
+    yield put(actions.onCreateHoursFailed(error));
+  }
+}
 
 export function* watchHours() {
   yield all([
     takeLatest(constants.HOURS_ON_GET_ALL_REQUESTED, hoursGetAll),
+    takeLatest(constants.HOURS_ON_CREATE_REQUESTED,hoursCreate)
   ]);
 }
